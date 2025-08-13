c2d9b25f5495c6b8d1e925eb01606403b63688ba
Nick384Developer <kooyonicholas@gmail.com>
2025-08-13 15:31:18 +0300

    updated department

--- contacts/views.py
+++ contacts/views.py
@@ -3,6 +3,4 @@ from .models import ContactInfo
 
 def contact_view(request):
     contact_info = ContactInfo.objects.first()
-    return render(request, 'contacts/contact.html', {
-        'contact_info': contact_info
-    })
+    return render(request, 'contacts/contact.html')
